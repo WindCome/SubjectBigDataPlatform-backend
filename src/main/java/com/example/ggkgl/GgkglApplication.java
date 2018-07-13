cd95c82a6a06a90e6bcc2c2375bbf73b7e5b4a45
alex <1219296951@qq.com>
2018-07-13 19:00:55 +0800

    Initial commit

---
+++ src/main/java/com/example/ggkgl/GgkglApplication.java
@@ -0,0 +1,12 @@
+package com.example.ggkgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgkglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgkglApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/ggkgl/GgkglApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ggkgl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GgkglApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
